f393b946180c3d4b6e11b92c7947cd2de1916925
DawidKrupski <dawid.krupski323@gmail.com>
2023-03-05 13:57:23 +0100

    React Hooks refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,112 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AddContact } from './AddContact/AddContact';
 import { Filter } from './ContactList/Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
 
-export class App extends React.PureComponent {
-  state = {
-    contacts: [
-      { id: 1, name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 2, name: 'Hermione Kline', number: '443-89-12' },
-      { id: 3, name: 'Eden Clements', number: '645-17-79' },
-      { id: 4, name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  handleSubmit = evt => {
+export const App = () => {
+  const [contact, setContact] = useState([]);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const handleSubmit = evt => {
     evt.preventDefault();
-    let contactExist = this.state.contacts.find(
-      contact => contact.name === this.state.name
-    );
-    const { contacts } = this.state;
-    const lastid =
-      contacts.length > 0
-        ? Math.max(...contacts.map(contact => contact.id))
-        : 0;
-
+    let contactExist = contact.find(contact => contact.name === name);
     if (!contactExist) {
-      this.setState(state => ({
-        contacts: state.contacts.concat({
-          name: state.name,
-          number: state.number,
-          id: lastid + 1,
-        }),
-        name: '',
-        number: '',
-      }));
-    } else alert(`${this.state.name} is alredy in contacts.`);
+      const newContact = contact.concat({
+        name: name,
+        number: number,
+        id: nanoid(),
+      });
+      setContact(newContact);
+    } else alert(`${name} is alredy in contacts.`);
   };
 
-  handleNameInput = evt => {
-    this.setState({
-      name: evt.target.value,
-    });
+  const handleNameInput = evt => {
+    const newName = evt.target.value;
+    setName(newName);
   };
 
-  handleNumberInput = evt => {
-    this.setState({
-      number: evt.target.value,
-    });
+  const handleNumberInput = evt => {
+    const newNumber = evt.target.value;
+    setNumber(newNumber);
+    console.log(number);
   };
 
-  deleteContact = id => {
-    this.setState(state => ({
-      contacts: state.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    const contactDelete = contact.filter(contact => contact.id !== id);
+    setContact(contactDelete);
   };
 
-  handleInput = evt => {
+  const handleInput = evt => {
     const text = evt.target.value;
-    this.setState({
-      filter: text,
-    });
+    setFilter(text);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      const contactListStorage = JSON.stringify(this.state.contacts);
-      window.localStorage.setItem('contact-list', contactListStorage);
-    }
-  }
+  return (
+    <div
+      style={{
+        marginLeft: 'calc(50% - 150px)  ',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <AddContact
+        name={name}
+        number={number}
+        handleNameInput={handleNameInput}
+        handleNumberInput={handleNumberInput}
+        handleSubmit={handleSubmit}
+      />
 
-  componentDidMount() {
-    const list = window.localStorage.getItem('contact-list');
-    if (!list) return;
-
-    try {
-      this.setState(state => ({
-        contacts: JSON.parse(list),
-      }));
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  render() {
-    return (
-      <div
-        style={{
-          marginLeft: 'calc(50% - 150px)  ',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <AddContact
-          name={this.state.name}
-          number={this.state.number}
-          handleNameInput={this.handleNameInput}
-          handleNumberInput={this.handleNumberInput}
-          handleSubmit={this.handleSubmit}
-        />
-
-        <h2>Contacts</h2>
-        <Filter handleInput={this.handleInput} />
-        <ContactList
-          contacts={this.state.contacts}
-          deleteContact={this.deleteContact}
-          filter={this.state.filter}
-          key={this.state.contacts.id}
-        />
-      </div>
-    );
-  }
-}
+      <h2>Contacts</h2>
+      <Filter handleInput={handleInput} />
+      <ContactList
+        contact={contact}
+        deleteContact={deleteContact}
+        filter={filter}
+      />
+    </div>
+  );
+};
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
-export class ContactList extends React.Component {
-  render() {
-    const { contacts, filter, deleteContact } = this.props;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
+export const ContactList = ({ contact, filter, deleteContact }) => {
+  const filteredContacts = contact.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-    return (
-      <>
-        {filteredContacts.map(({ name, number, id }) => (
-          <li key={id}>
-            {name}: {number}
-            <button onClick={() => deleteContact(id)}>Delete</button>
-          </li>
-        ))}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {filteredContacts.map(({ name, number, id }) => (
+        <li key={id}>
+          {name}: {number}
+          <button onClick={() => deleteContact(id)}>Delete</button>
+        </li>
+      ))}
+    </>
+  );
+};
